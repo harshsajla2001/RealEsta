9e4391b47b1d467a8539949d2e5c49a23b55658d
harsh sajla <harsh.sajla2001@gmail.com>
2022-08-03 21:11:03 +0530

    added alt massage

--- components/searchFilters.jsx
+++ components/searchFilters.jsx
@@ -100,7 +100,7 @@ export default function SearchFilters() {
                 ))}
                 {!loading && !locationData?.length && (
                   <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
-                    <Image src={noresult} />
+                    <Image src={noresult} alt="Image Not Found Refresh Or Report bug"/>
                     <Text fontSize='xl' marginTop='3'>
                       Waiting to search!
                     </Text>
